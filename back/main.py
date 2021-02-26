35c2254c5df4515afaf24939d5fe1734ccc85a0f
chamegashi <msymsy0693wrb@gmail.com>
2021-02-26 20:39:37 +0900

    add socket

--- back/main.py
+++ back/main.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import os
 
 
 app = Flask(__name__, static_folder='../dist/static', template_folder='../dist')
+
+
+@app.route('/get_test_data', methods=["POST"])
+def get_test_data():
+    return jsonify({"test_index_data": "いい感じのデータ: " + request.form["test_post_data"]})
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
