0a8b1de9367e631a44c20ac50befef418efed234
chamegashi <msymsy0693wrb@gmail.com>
2021-02-26 21:49:02 +0900

    test

--- back/main.py
+++ back/main.py
@@ -13,12 +13,12 @@ def get_test_data():
 
 @app.route('/youtube_data', methods=["POST"])
 def get_youtube_data():
-    return jsonify({"youtube_data": "いい感じのデータ: " + request.form["test_post_data"]})
+    return jsonify({"youtube_data": "youtube のいい感じのデータ: " + request.form["test_post_data"]})
 
 
 @app.route('/twitter_data', methods=["POST"])
 def get_twitter_data():
-    return jsonify({"twitter_data": "いい感じのデータ: " + request.form["test_post_data"]})
+    return jsonify({"twitter_data": "twitter のいい感じのデータ: " + request.form["test_post_data"]})
 
 
 @app.route('/', defaults={'path': ''})
