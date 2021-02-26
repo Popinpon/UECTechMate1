817aad516505bc89eeef2f68dee811d76fdc13e1
Takata1128 <takata_s1128@ezweb.ne.jp>
2021-02-26 21:00:13 +0900

    twitterとyoutubeのendpoint

--- back/main.py
+++ back/main.py
@@ -2,17 +2,30 @@ from flask import Flask, render_template, request, jsonify
 import os
 
 
-app = Flask(__name__, static_folder='../dist/static', template_folder='../dist')
+app = Flask(__name__, static_folder='../dist/static',
+            template_folder='../dist')
 
 
 @app.route('/get_test_data', methods=["POST"])
 def get_test_data():
     return jsonify({"test_index_data": "いい感じのデータ: " + request.form["test_post_data"]})
 
+
+@app.route('/youtube_data', methods=["POST"])
+def get_youtube_data():
+    pass
+
+
+@app.route('/twitter_data', methods=["POST"])
+def get_twitter_data():
+    pass
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
