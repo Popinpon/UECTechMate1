80a63714fb09d2ddbcc633a8e56cee851aaa863f
chamegashi <msymsy0693wrb@gmail.com>
2021-02-28 04:43:59 +0900

    stash

--- front/src/components/RoomInfoPage.vue
+++ front/src/components/RoomInfoPage.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="bg-black text-white">
 
+    <!-- <p class="text-center font-sans text-7xl mb-9">Zoom View</p>
+
+    <div class="relative">
+      <div class="w-3/5 absolute translate-x-3/4">
+        <img src="static\img\zoom_test.png">
+      </div>
+      <div class="flex justify-center">
+        <div class="w-3/5">
+          <img src="static\img\zoom_test.png">
+        </div>
+      </div>
+      <div class="w-3/5 absolute -translate-x-3/4">
+        <img src="static\img\zoom_test.png">
+      </div>
+    </div>
+ -->
     <p class="text-center font-sans text-7xl mb-9">Responce</p>
     <div class="flex justify-center">
       <div class="w-4/5 flex justify-center mb-6">
@@ -37,77 +53,6 @@
         </div>
       </div>
     </div>
-
-
-    <!-- <p class="text-center font-sans text-7xl mb-9">Twtter Responce</p>
-    <div class="flex justify-center">
-      <div class="w-4/5 flex justify-center mb-6">
-        <div class="w-1/3 h-20 bg-green-600 rounded-full m-1 p-4">
-          <p class="font-mono text-5xl text-white extrabold text-center">ホールA</p>
-        </div>
-        <div class="w-1/3 h-20 bg-pink-600 rounded-full m-1 p-4">
-          <p class="font-mono text-5xl text-white extrabold text-center">ホールB</p>
-        </div>
-        <div class="w-1/3 h-20 bg-purple-600 rounded-full m-1 p-4">
-          <p class="font-mono text-5xl text-white extrabold text-center">ホールC</p>
-        </div>
-      </div>
-    </div>
-    <div class="flex justify-center mb-6">
-      <div class="w-4/5 flex justify-center mb-6">
-        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-green-200">
-          <div v-for="tweetA in twitterRoomAData" :key="tweetA.id">
-            <TwitterCard v-bind="tweetA"/>
-          </div>
-        </div>
-        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-pink-200">
-          <div v-for="tweetB in twitterRoomBData" :key="tweetB.id">
-            <TwitterCard v-bind="tweetB"/>
-          </div>
-        </div>
-        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-purple-200">
-          <div v-for="tweetC in twitterRoomCData" :key="tweetC.id">
-            <TwitterCard v-bind="tweetC"/>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
-<!-- 
-    <p class="text-center font-sans text-7xl mb-9">Youtube Responce</p>
-    <div class="flex justify-center">
-      <div class="w-4/5 flex justify-center mb-6">
-        <div class="w-1/3 h-20 bg-green-600 rounded-full m-1 p-4">
-          <p class="font-mono text-5xl text-white extrabold text-center">ホールA</p>
-        </div>
-        <div class="w-1/3 h-20 bg-pink-600 rounded-full m-1 p-4">
-          <p class="font-mono text-5xl text-white extrabold text-center">ホールB</p>
-        </div>
-        <div class="w-1/3 h-20 bg-purple-600 rounded-full m-1 p-4">
-          <p class="font-mono text-5xl text-white extrabold text-center">ホールC</p>
-        </div>
-      </div>
-    </div>
-    <div class="flex justify-center">
-      <div class="w-4/5 flex justify-center mb-6">
-        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-green-200">
-          <div v-for="commentA in youtubeRoomAData" :key="commentA.id">
-            <YoutubeCard v-bind="commentA"/>
-          </div>
-        </div>
-        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-pink-200">
-          <div v-for="commentB in youtubeRoomBData" :key="commentB.id">
-            <YoutubeCard v-bind="commentB"/>
-          </div>
-        </div>
-        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-purple-200">
-          <div v-for="commentC in youtubeRoomCData" :key="commentC.id">
-            <YoutubeCard v-bind="commentC"/>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
   </div>
 </template>
 
@@ -122,12 +67,6 @@ export default Vue.extend({
   data() {
     return {
       imageSrc: "/static/img/no_image.jpg",
-      // youtubeRoomAData: [],
-      // youtubeRoomBData: [],
-      // youtubeRoomCData: [],
-      // twitterRoomAData: [],
-      // twitterRoomBData: [],
-      // twitterRoomCData: [],
       roomAData: [],
       roomBData: [],
       roomCData: [],
@@ -162,37 +101,6 @@ export default Vue.extend({
         list.push(data);
         if (list.length >= 10) list.shift();
       }
-
-      // if (data.type == "twitter") {
-      //   if (data.room_type == "A") {
-      //     const list = this.twitterRoomAData;
-      //     list.push(data);
-      //     if (list.length >= 10) list.shift();
-      //   } else if (data.room_type == "B") {
-      //     const list = this.twitterRoomBData;
-      //     list.push(data);
-      //     if (list.length >= 10) list.shift();
-      //   } else {
-      //     const list = this.twitterRoomCData;
-      //     list.push(data);
-      //     if (list.length >= 10) list.shift();
-      //   }
-      // } else {
-      //   if (data.room_type == "A") {
-      //     const list = this.youtubeRoomAData;
-      //     list.push(data);
-      //     if (list.length >= 10) list.shift();
-      //   } else if (data.room_type == "B") {
-      //     const list = this.youtubeRoomBData;
-      //     list.push(data);
-      //     if (list.length >= 10) list.shift();
-      //   } else {
-      //     const list = this.youtubeRoomCData;
-      //     list.push(data);
-      //     if (list.length >= 10) list.shift();
-      //   }
-      // }
-
     },
   },
 });
