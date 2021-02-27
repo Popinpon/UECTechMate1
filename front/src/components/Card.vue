726a9ad27b07a91ab92c28a39b8b52556a7fc37e
chamegashi <msymsy0693wrb@gmail.com>
2021-02-27 22:12:21 +0900

    made calendar

--- front/src/components/Card.vue
+++ front/src/components/Card.vue
@@ -4,7 +4,10 @@
       <img v-if="image" class="w-20 rounded-full border-2 border-gray-300" alt="image" :src="image">
       <img v-else class="w-20 rounded-full border-2 border-gray-300" alt="image" src="/static/img/no_image.jpg">
       <div id="header-text" class="leading-5 ml-6 sm">
-        <h4 class="text-xl font-semibold">{{name}}</h4>
+        <div class="flex justify-start">
+          <h4 class="text-xl font-semibold">{{name}}</h4>
+          <a v-if="twitter" class="p-1" :href="twitter"><img class="w-5" src="/static/img/twitter.png"></a>
+        </div>
         <h5 class="font-semibold  text-blue-600">{{job}}</h5>
       </div>
     </div>
@@ -23,6 +26,7 @@ export default Vue.extend({
     job: String,
     text: String,
     image: String,
+    twitter: String,
   },
 })
 </script>
--- front/src/components/WideCard.vue
+++ front/src/components/WideCard.vue
@@ -9,6 +9,16 @@
       <span class="italic text-gray-600 ml-8">{{text}}</span>
     </div>
   </div>
+  <div v-else-if="time === 65" class="h-38 bg-white border-2 border-gray-300 p-6 rounded-md tracking-wide shadow-lg">
+    <div id="header" class="flex items-center mb-4 mt-4">
+      <div id="header-text" class="leading-5 sm">
+        <h4 class="text-4xl font-semibold">{{title}}</h4>
+      </div>
+    </div>
+    <div>
+      <span class="italic text-gray-600 ml-8">{{text}}</span>
+    </div>
+  </div>
   <div v-else-if="time === 60" class="h-40 bg-white border-2 border-gray-300 p-6 rounded-md tracking-wide shadow-lg">
     <div id="header" class="flex items-center mb-4 mt-4">
       <div id="header-text" class="leading-5 sm">
