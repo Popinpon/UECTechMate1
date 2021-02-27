89d45c4ec21f2c98e357746e4e2fdf62b86f02ec
chamegashi <msymsy0693wrb@gmail.com>
2021-02-27 12:27:27 +0900

    rebuild

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,20 +3,21 @@
     <div class="flex justify-center">
       <img alt="Vue logo" src="static/img/logo.png">
     </div>
-    <CalendarPage/>
-    <!-- <Hallo/> -->
+    <!-- <CalendarPage/> -->
+    <Hallo/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import CalendarPage from './components/CalendarPage.vue'
-// import Hallo from './components/HelloWorld'
+// import CalendarPage from './components/CalendarPage.vue'
+import Hallo from './components/HelloWorld.vue'
 
 export default Vue.extend({
   name: 'App',
   components: {
-    CalendarPage,
+    // CalendarPage,
+    Hallo,
   }
 })
 </script>
