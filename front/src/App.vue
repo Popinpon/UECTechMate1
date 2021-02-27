7c43a8d7a35efa7f30ab570f610c95b0e39cf58d
chamegashi <msymsy0693wrb@gmail.com>
2021-02-28 03:11:20 +0900

    twitter board update

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
     <div class="relative bg-white">
       <div class="max-w-7xl mx-auto px-4 sm:px-6">
-        <div class="flex justify-between items-center border-b-2 border-gray-100 py-4 md:justify-start md:space-x-10">
+        <div class="flex justify-between items-center border-b-2 border-gray-100 py-2 md:justify-start md:space-x-10">
           <div class="flex justify-start lg:w-0 lg:flex-1">
             <a href="#">
-              <img class="h-8 w-auto sm:h-14" src="static/img/pro.png">
+              <img class="h-16 w-auto rounded-full" src="static/img/team_logo.png">
             </a>
           </div>
           <nav class="hidden md:flex space-x-10">
--- front/src/components/RoomInfoPage.vue
+++ front/src/components/RoomInfoPage.vue
@@ -14,7 +14,7 @@
         </div>
       </div>
     </div>
-    <div class="flex justify-center">
+    <div class="flex justify-center mb-6">
       <div class="w-4/5 flex justify-center mb-6">
         <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-green-200">
           <div v-for="tweetA in twitterRoomAData" :key="tweetA.id">
@@ -33,10 +33,37 @@
         </div>
       </div>
     </div>
-    <div class="bg-blue-900 text-white">
-      <h3>Youtube Responce</h3>
-      <div v-for="post in youtubeRoomCData" :key="post.id">
-        <YoutubeCard v-bind="post"/>
+    <p class="text-center font-sans text-7xl mb-9">Youtube Responce</p>
+    <div class="flex justify-center">
+      <div class="w-4/5 flex justify-center mb-6">
+        <div class="w-1/3 h-20 bg-green-600 rounded-full m-1 p-4">
+          <p class="font-mono text-5xl text-white extrabold text-center">ホールA</p>
+        </div>
+        <div class="w-1/3 h-20 bg-pink-600 rounded-full m-1 p-4">
+          <p class="font-mono text-5xl text-white extrabold text-center">ホールB</p>
+        </div>
+        <div class="w-1/3 h-20 bg-purple-600 rounded-full m-1 p-4">
+          <p class="font-mono text-5xl text-white extrabold text-center">ホールC</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-center">
+      <div class="w-4/5 flex justify-center mb-6">
+        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-green-200">
+          <div v-for="commentA in youtubeRoomAData" :key="commentA.id">
+            <YoutubeCard v-bind="commentA"/>
+          </div>
+        </div>
+        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-pink-200">
+          <div v-for="commentB in youtubeRoomBData" :key="commentB.id">
+            <YoutubeCard v-bind="commentB"/>
+          </div>
+        </div>
+        <div class="w-1/3 bg-white border-2 rounded-md border-gray-300 m-1 bg-purple-200">
+          <div v-for="commentC in youtubeRoomCData" :key="commentC.id">
+            <YoutubeCard v-bind="commentC"/>
+          </div>
+        </div>
       </div>
     </div>
   </div>
