dede4ffea22774cc32e2416b500f1f1b824eb227
Shimizu Daiki <sisimimizuzu@gmail.com>
2021-02-28 14:51:11 +0900

    screenshot

--- back/capZoom.py
+++ back/capZoom.py
@@ -3,7 +3,11 @@ import time,os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import setting
-
+import requests
+from PIL import Image
+import json
+import base64
+from io import BytesIO
 repeatTime=5
 
 options=Options()
@@ -16,20 +20,54 @@ elif os.name == 'posix':
 userdata_dir = r'C:SelenUserData'
 options.add_argument('--user-data-dir=' + userdata_dir)
 driver = webdriver.Chrome(chrome_options=options,executable_path="./chromedriver_win32/chromedriver")
-zoomURL="https://uec-tokyo.zoom.us/j/95142903465?pwd=NkIyRzVaZkxGYUNLV0lTOU5vNkFQdz09"
-driver.get(zoomURL)
-time.sleep(5)
-element=driver.find_element_by_xpath('//*[@id="zoom-ui-frame"]/div[2]/div/div[2]/h3[2]/a')
+
+driver.get("http://localhost:9999")
+
+time.sleep(1)
+passcode="0023269450"
+meeting="941 1475 9766"
+# time.sleep(5)
+
+clearBtn='//*[@id="clear_all"]'
+element=driver.find_element_by_xpath(clearBtn)
+element.click()
+time.sleep(0.5)
+element=driver.find_element_by_xpath('//*[@id="display_name"]')
+element.send_keys()
+time.sleep(0.1)
+element=driver.find_element_by_xpath('//*[@id="meeting_number"]')
+element.send_keys(meeting)
+element=driver.find_element_by_xpath('//*[@id="meeting_pwd"]')
+time.sleep(1)
+element.send_keys(passcode)
+element=driver.find_element_by_xpath('//*[@id="join_meeting"]')
 time.sleep(1)
 element.click()
-
-os.path.join(os.path.dirname(os.path.abspath(__file__)), "image/screen.png")
-
-
+time.sleep(3)
+driver.switch_to.window(driver.window_handles[1])
 room_names=list(setting.YT_URL)
 for count in range(repeatTime):
     for i,room in enumerate(room_names):
         time.sleep(1)
-        driver.save_screenshot("../dist/static/img/"+room+str(count)+".png")
+    #    element=driver.find_element_by_xpath('//*[@id="wc-container-left"]/div[4]/div/div[2]/canvas')
+        imgpath='../dit/static/img/'+room+str(count)+".png"
+        png=driver.save_screenshot(imgpath)
         time.sleep(5)
-driver.quit()
+
+
+        img = Image.open(os.path.abspath(imgpath))
+        buffered = BytesIO()
+        img.save(buffered, format="JPEG")
+        img_byte = buffered.getvalue()
+        img_base64 = base64.b64encode(img_byte)
+
+        img_str = img_base64.decode('utf-8')
+        json_data = {
+            "room_type": "A",
+            "img": img_str
+        }
+        r = requests.post("http://localhost:5000/save_img",
+                          json=json.dumps(json_data))
+        print(r.json())
+#time.sleep(5)
+#driver.quit()
